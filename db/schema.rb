3bfb3ffe6ecc59fd2a7de17d22394b6871bef5c2
Adam Edelstein <asedelst@edelBook.local>
2019-10-31 13:56:54 -0500

    more update for css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_175508) do
+ActiveRecord::Schema.define(version: 2019_10_31_185404) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "Symbol"
