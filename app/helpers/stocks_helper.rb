68c4994bc78b55dc388ca4b751c995b1c337b1f8
Adam Edelstein <asedelst@edelBook.local>
2019-10-31 16:14:00 -0500

    close to RoR completion

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -2,7 +2,7 @@ module StocksHelper
   require 'digest/md5'
 
   def stock_image(stock)
-    color = Digest::MD5.hexdigest(app.name)[0..5]
-    "https://dummyimage.com/300x300/#{color}/ffffff.png&text=#{stock.name}"
+    color = Digest::MD5.hexdigest(stock.Name)[0..5]
+    "https://dummyimage.com/300x300/#{color}/ffffff.png&text=#{stock.Name}"
   end
 end
--- app/views/stocks/_stock.json.jbuilder
+++ app/views/stocks/_stock.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! stock, :id, :Name, :Symbol, :Industry :Sector :Exchange :SummaryQuote :MarketCap :IPOYear :LastSale :created_at, :updated_at
+json.extract! stock, :id, :Name, :Symbol, :Industry :Sector :Exchange :SummaryQuote :MarketCap :IPOyear :LastSale :created_at, :updated_at
 json.url app_url(app, format: :json)
